24242fdfd4f5d708305c1bcf91ada4e7115dd6fe
vivicodes02 <agathadaniel0206@gmailcom>
2025-12-05 16:13:33 +0300

    remove text

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   </div>
   <h3 className="text-gray-900 dark:text-white mt-5 text-base font-medium tracking-tight ">Writes upside-down</h3>
   <p className="text-gray-500 dark:text-gray-400 mt-2 text-sm ">
-    The Zero Gravity Pen can be used to write in any orientation, 
+    The Zero Gravity Pen can be used to write in any , 
   </p>
 </div>
   )
